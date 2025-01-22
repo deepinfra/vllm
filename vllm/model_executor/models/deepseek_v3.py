3e9f544af069bed238d6851489d570c1010848ff
Yessen Kanapin <yessen@deepinfra.com>
2025-01-21 16:54:55 -0800

    port v2 changes unto v3

--- vllm/model_executor/models/deepseek_v3.py
+++ vllm/model_executor/models/deepseek_v3.py
@@ -27,7 +27,7 @@ from torch import nn
 from transformers import PretrainedConfig
 
 from vllm.attention import Attention, AttentionMetadata
-from vllm.config import CacheConfig, VllmConfig
+from vllm.config import CacheConfig, ModelConfig, VllmConfig
 from vllm.distributed import (get_pp_group,
                               get_tensor_model_parallel_world_size,
                               tensor_model_parallel_all_reduce)
@@ -44,7 +44,8 @@ from vllm.model_executor.layers.rotary_embedding import get_rope
 from vllm.model_executor.layers.sampler import SamplerOutput, get_sampler
 from vllm.model_executor.layers.vocab_parallel_embedding import (
     ParallelLMHead, VocabParallelEmbedding)
-from vllm.model_executor.model_loader.weight_utils import default_weight_loader
+from vllm.model_executor.model_loader.weight_utils import (
+    default_weight_loader, maybe_remap_kv_scale_name)
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.sequence import IntermediateTensors
 
@@ -332,6 +333,337 @@ class DeepseekV3Attention(nn.Module):
         output, _ = self.o_proj(attn_output)
         return output
 
+class DeepseekV3MLAAttention(nn.Module):
+    """
+    Main reference: DeepseekV2 paper, and FlashInfer Implementation
+    (https://github.com/flashinfer-ai/flashinfer/pull/551).
+
+    Deepseek's MLA attention works the following way:
+    * Use a single latent vector to represent the entire KV cache.
+    * The attention "simulates" a multi-head attention, while the compute is
+      similar to multi-query attention.
+    * The dataflow is as follows,
+
+        * B: batch/sequence length
+        * H: hidden size
+        * N: number of attention heads
+        * Lq: latent dimension for Q
+        * Lkv: latent dimension for K/V
+        * P: nope dimension, P+R is the actual head_dim in common attention.
+        * R: rope dimension, this slide of the head_dim goes through rope.
+        * V: V head dim.
+
+        # The reconstructed way, as implemented in DeepseekV2Attention:
+        1. The hidden states (B, H) are projected down into q_latent (B, Lq) and
+           kv_latent (B, Lkv+R).
+        2. The kv_latent is split into kv_a (B, Lkv) and k_pe (B, R). q_latent
+           and kv_a are normalized.
+        3. The q_latent and kv_a are then projected up into the multi-head
+           version. q_latent goes from (B, Lq) to (B, N(P+R)) included the rope
+           dimension, which is split into q_nope (B, N, P) and q_pe (B, N, R).
+           kv_a goes from (B, Lkv) to (B, N(P+V)) which has the nope dimensions
+           for K and V, which is split into k_nope (B, N, P) and v (B, N, V).
+        3. q_pe, k_pe are then passed through rotary embeddings.
+        4. q (B, N, (P+R)) and k (B, N, (P+R)) matrices are assembled from
+           q_nope, q_pe, k_nope, k_pe.
+        5. Attention is computued with q, k, v.
+        6. The KV cache is updated with the new entries k (B, N, (P+R)) and v
+           (B, N, V), we pad the head dim to 256 so that the KV cache has
+           consistent shape and works with a typical cache implementation.
+        7. The attention computation returns (B, N, V), which is projected back
+           to (B, H) using out projection.
+
+        # The recommended way, as described in the paper:
+        1. The hidden states (B, H) are projected down into q_latent (B, Lq) and
+           kv_latent (B, Lkv+R).
+        2. The kv_latent is split into kv_a (B, Lkv) and k_pe (B, R). q_latent
+           and kv_a are normalized.
+        3. Here's the change, we do not perform up the full up projection for
+           q_latent, and there is no up projection at all for kv_a. This is
+           achieved by the technique of "weight absorption". The paper says
+           "Fortunately, due to the associative law of matrix multiplication,
+           we can absorb WUK into WUQ, and WUV into WO"
+           * The q up projection turns (B, Lq) into (B, N(P+R)), we split it
+             into W_UQ (Lq, N, P) and W_QR (Lq, N, R).
+           * The kv_a up projection turns (B, Lkv) into (B, N(P+V)), we split it
+             into W_UK (Lkv, N, P) and W_UV (Lkv, N, V).
+           * The out projection shape W_O (V, H)turns (B, N, V) into (B, H).
+           * We can precompute the product of W_UQ and W_UK into
+             W_UQ_UK (Lq, N, Lkv), which is possible due to QK^T operation in
+             attention.
+           * We can precompute the product of W_UV and W_O into
+             W_UV_O (N, Lkv, H), which is possible due to V@O as the
+             "epilogue" of attention
+        4. We still need to compute q_pe (B, N, R) by applying W_QR to q_latent.
+           The rotary embeddingss still need to be applied to q_pe and k_pe.
+        5. By applying W_UQ_UK to q_latent, we have the new q_nope of shape
+           (B, N, Lkv).
+        6. q (B, N, (Lkv+R)), k (B, (Lkv+R)) are assembled from q_nope, q_pe,
+           kv_a, k_pe. v (B, Lkv) is exactly the same vector as kv_a.
+        6. The attention is computed with q, k, v. Note that we just performed
+           a MQA attention with (LKv+R) as our head dim.
+        7. The KV cache is updated using the new entries k (B, N, (Lkv+R)),
+           which included the v and rope values.
+        8. The attention computation returns (B, N, Lkv), which is projected
+           back to (B, H) using W_UV_O.
+
+    From @tsu-bin's calculation, we only want to use the absorption technique
+    for decode. The prefill algorithm should still use the up-projected MHA
+    for less flops and memory usage.
+    """
+
+    def __init__(
+        self,
+        config: PretrainedConfig,
+        hidden_size: int,
+        num_heads: int,
+        qk_nope_head_dim: int,
+        qk_rope_head_dim: int,
+        v_head_dim: int,
+        q_lora_rank: Optional[int],
+        kv_lora_rank: int,
+        rope_theta: float = 10000,
+        rope_scaling: Optional[Dict[str, Any]] = None,
+        max_position_embeddings: int = 8192,
+        cache_config: Optional[CacheConfig] = None,
+        quant_config: Optional[QuantizationConfig] = None,
+        prefix: str = "",
+    ) -> None:
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.qk_nope_head_dim = qk_nope_head_dim
+        self.qk_rope_head_dim = qk_rope_head_dim
+        self.qk_head_dim = qk_nope_head_dim + qk_rope_head_dim
+        self.v_head_dim = v_head_dim
+
+        self.q_lora_rank = q_lora_rank
+        self.kv_lora_rank = kv_lora_rank
+
+        self.num_heads = num_heads
+        tp_size = get_tensor_model_parallel_world_size()
+        assert num_heads % tp_size == 0
+        self.num_local_heads = num_heads // tp_size
+
+        self.scaling = self.qk_head_dim**-0.5
+        self.rope_theta = rope_theta
+        self.max_position_embeddings = max_position_embeddings
+
+        if self.q_lora_rank is not None:
+            self.q_a_proj = ReplicatedLinear(self.hidden_size,
+                                             self.q_lora_rank,
+                                             bias=False,
+                                             quant_config=quant_config,
+                                             prefix=f"{prefix}.q_a_proj")
+            self.q_a_layernorm = RMSNorm(self.q_lora_rank,
+                                         eps=config.rms_norm_eps)
+            self.q_b_proj = ColumnParallelLinear(q_lora_rank,
+                                                 self.num_heads *
+                                                 self.qk_head_dim,
+                                                 bias=False,
+                                                 quant_config=quant_config,
+                                                 prefix=f"{prefix}.q_b_proj")
+        else:
+            self.q_proj = ColumnParallelLinear(self.hidden_size,
+                                               self.num_heads *
+                                               self.qk_head_dim,
+                                               bias=False,
+                                               quant_config=quant_config,
+                                               prefix=f"{prefix}.q_proj")
+
+        self.kv_a_proj_with_mqa = ReplicatedLinear(
+            self.hidden_size,
+            self.kv_lora_rank + self.qk_rope_head_dim,
+            bias=False,
+            quant_config=quant_config,
+            prefix=f"{prefix}.kv_a_proj_with_mqa")
+        self.kv_a_layernorm = RMSNorm(self.kv_lora_rank,
+                                      eps=config.rms_norm_eps)
+        self.kv_b_proj = ColumnParallelLinear(
+            self.kv_lora_rank,
+            self.num_heads * (self.qk_nope_head_dim + self.v_head_dim),
+            bias=False,
+            quant_config=quant_config,
+            prefix=f"{prefix}.kv_b_proj")
+        self.o_proj = RowParallelLinear(self.num_heads * self.v_head_dim,
+                                        self.hidden_size,
+                                        bias=False,
+                                        quant_config=quant_config,
+                                        prefix=f"{prefix}.o_proj")
+
+        rope_scaling["rope_type"] = 'deepseek_yarn'
+        self.rotary_emb = get_rope(qk_rope_head_dim,
+                                   rotary_dim=qk_rope_head_dim,
+                                   max_position=max_position_embeddings,
+                                   base=rope_theta,
+                                   rope_scaling=rope_scaling,
+                                   is_neox_style=False)
+        if rope_scaling:
+            mscale_all_dim = rope_scaling.get("mscale_all_dim", False)
+            scaling_factor = rope_scaling["factor"]
+            mscale = yarn_get_mscale(scaling_factor, float(mscale_all_dim))
+            self.scaling = self.scaling * mscale * mscale
+
+        self.attn = Attention(num_heads=self.num_local_heads,
+                              head_size=self.kv_lora_rank,
+                              scale=self.scaling,
+                              num_kv_heads=1,
+                              cache_config=cache_config,
+                              quant_config=quant_config,
+                              prefix=f"{prefix}.attn",
+                              use_mla=True)
+
+        # To be computed during weight loading
+        # self.W_QR = None
+        # self.W_UQ_UK = None
+        # self.W_UV_O = None
+
+        kv_b_proj_weight = self.kv_b_proj.weight.T
+        assert kv_b_proj_weight.shape == (
+            self.kv_lora_rank, self.num_local_heads *
+            (self.qk_nope_head_dim + self.v_head_dim)), (
+                f"{kv_b_proj_weight.shape=}, "
+                f"{self.kv_lora_rank=}, "
+                f"{self.num_local_heads=}, "
+                f"{self.qk_nope_head_dim=}, "
+                f"{self.v_head_dim=}")
+        kv_b_proj_weight = kv_b_proj_weight.view(
+            self.kv_lora_rank,
+            self.num_local_heads,
+            self.qk_nope_head_dim + self.v_head_dim,
+        )
+        self.W_UK, self.W_UV = kv_b_proj_weight.split(
+            [self.qk_nope_head_dim, self.v_head_dim], dim=-1)
+
+        self.prefix = prefix
+        self.debug_layer_idx = int(self.prefix.split(".")[-2])
+
+    def forward(
+        self,
+        positions: torch.Tensor,
+        hidden_states: torch.Tensor,
+        kv_cache: torch.Tensor,
+        attn_metadata: AttentionMetadata,
+    ) -> torch.Tensor:
+        # TODO(simon): support append/chunked prefill by two kernels,
+        # or using the decode kernel somehow.
+        if attn_metadata.prefill_metadata and attn_metadata.decode_metadata:
+            raise ValueError(
+                "Chunked prefill is not supported when MLA is enabled.")
+        if attn_metadata.prefill_metadata:
+            return self.forward_prefill(positions, hidden_states, kv_cache,
+                                        attn_metadata)
+        if attn_metadata.decode_metadata:
+            return self.forward_decode(positions, hidden_states, kv_cache,
+                                       attn_metadata)
+
+    def forward_prefill(
+        self,
+        positions: torch.Tensor,
+        hidden_states: torch.Tensor,
+        kv_cache: torch.Tensor,
+        attn_metadata: AttentionMetadata,
+    ) -> torch.Tensor:
+        if self.q_lora_rank is not None:
+            q = self.q_a_proj(hidden_states)[0]
+            q = self.q_a_layernorm(q)
+            q = self.q_b_proj(q)[0].view(-1, self.num_local_heads,
+                                         self.qk_head_dim)
+        else:
+            q = self.q_proj(hidden_states)[0].view(-1, self.num_local_heads,
+                                                   self.qk_head_dim)
+
+        _, q_pe = q.split([self.qk_nope_head_dim, self.qk_rope_head_dim],
+                          dim=-1)
+        latent_cache = self.kv_a_proj_with_mqa(hidden_states)[0]
+        kv_a, _ = latent_cache.split(
+            [self.kv_lora_rank, self.qk_rope_head_dim], dim=-1)
+        latent_cache = latent_cache.unsqueeze(1)
+        kv_a = self.kv_a_layernorm(kv_a.contiguous())
+        kv = self.kv_b_proj(kv_a)[0]
+        kv = kv.view(-1, self.num_local_heads,
+                     self.qk_nope_head_dim + self.v_head_dim)
+        k_nope, v = kv.split([self.qk_nope_head_dim, self.v_head_dim], dim=-1)
+        k_pe = latent_cache[:, :, self.kv_lora_rank:]
+        q_pe, k_pe = self.rotary_emb(positions, q_pe, k_pe)
+        q[..., self.qk_nope_head_dim:] = q_pe
+        k = torch.empty_like(q)
+        k[..., :self.qk_nope_head_dim] = k_nope
+        k[..., self.qk_nope_head_dim:] = k_pe
+
+        # HACK(simon): these need to be passed into the attention backend
+        # to write to the kv cache.
+        # TODO(simon): do we need to free these?
+        attn_metadata.extras = {
+            "kv_a":
+            kv_a.unsqueeze(1),  # restore the head dim to write to kv cache
+            "k_pe": k_pe,
+        }
+
+        attn_output = self.attn(q, k, v, kv_cache, attn_metadata)
+
+        # B(N'V) -> BH
+        output, _ = self.o_proj(attn_output)
+        return output
+
+    def forward_decode(
+        self,
+        positions: torch.Tensor,
+        hidden_states: torch.Tensor,
+        kv_cache: torch.Tensor,
+        attn_metadata: AttentionMetadata,
+    ) -> torch.Tensor:
+        B = hidden_states.shape[0]
+
+        # Apply UQ and QR.
+        if self.q_lora_rank is not None:
+            q = self.q_a_proj(hidden_states)[0]
+            q = self.q_a_layernorm(q)
+            q = self.q_b_proj(q)[0].view(-1, self.num_local_heads,
+                                         self.qk_head_dim)
+        else:
+            q = self.q_proj(hidden_states)[0].view(-1, self.num_local_heads,
+                                                   self.qk_head_dim)
+
+        q_nope, q_pe = q.split([self.qk_nope_head_dim, self.qk_rope_head_dim],
+                               dim=-1)
+
+        latent_cache = self.kv_a_proj_with_mqa(hidden_states)[0]
+        kv_a, k_pe = latent_cache.split(
+            [self.kv_lora_rank, self.qk_rope_head_dim], dim=-1)
+        kv_a = self.kv_a_layernorm(kv_a.contiguous())
+        k_pe = k_pe.unsqueeze(1)
+
+        q_pe, k_pe = self.rotary_emb(positions, q_pe, k_pe)
+        # Apply UK, q_nope (B, N, P) @ W_UK (L, N, P) -> (B, N, L)
+        q_nope = torch.einsum("bnp,lnp->bnl", q_nope, self.W_UK)
+        # essemble q, k, and v; here v is repurposed to represent k_pe
+
+        q = torch.empty((B, self.num_local_heads,
+                         self.kv_lora_rank + self.qk_rope_head_dim),
+                        dtype=q.dtype,
+                        device=q.device)
+        q[..., :self.kv_lora_rank] = q_nope
+        q[..., self.kv_lora_rank:] = q_pe
+
+        k = kv_a.unsqueeze(1)
+        # The padding is only used for kv storage.
+        v = torch.nn.functional.pad(
+            k_pe, [0, self.kv_lora_rank - self.qk_rope_head_dim], value=0)
+        assert k.numel() == v.numel(), f"{k.numel()=} != {v.numel()=}"
+
+        attn_metadata.debug_layer_idx = self.debug_layer_idx
+        attn_output = self.attn(q, k, v, kv_cache, attn_metadata)
+
+        # idk why but the attn_output is fp32
+        attn_output = attn_output.to(q.dtype)
+        # Apply UV, (B, N, L) @ W_UV (L, N, V) -> (B, N, V)
+        attn_output = torch.einsum("bnl,lnv->bnv", attn_output, self.W_UV)
+        attn_output = attn_output.reshape(
+            B, self.num_local_heads * self.v_head_dim)
+
+        output, _ = self.o_proj(attn_output)
+        return output
+
 
 class DeepseekV3DecoderLayer(nn.Module):
 
@@ -339,6 +671,7 @@ class DeepseekV3DecoderLayer(nn.Module):
         self,
         config: PretrainedConfig,
         prefix: str,
+        model_config: ModelConfig,
         cache_config: Optional[CacheConfig] = None,
         quant_config: Optional[QuantizationConfig] = None,
     ) -> None:
@@ -351,7 +684,11 @@ class DeepseekV3DecoderLayer(nn.Module):
         # DecoderLayers are created with `make_layers` which passes the prefix
         # with the layer's index.
         layer_idx = int(prefix.split(sep='.')[-1])
-        self.self_attn = DeepseekV3Attention(
+        if model_config.should_use_mla:
+            attn_cls = DeepseekV3MLAAttention
+        else:
+            attn_cls = DeepseekV3Attention
+        self.self_attn = attn_cls(
             config=config,
             hidden_size=self.hidden_size,
             num_heads=config.num_attention_heads,
@@ -641,6 +978,12 @@ class DeepseekV3ForCausalLM(nn.Module, SupportsPP):
                     # Skip loading extra bias for GPTQ models.
                     if name.endswith(".bias") and name not in params_dict:
                         continue
+
+                    # Remapping the name of FP8 kv-scale.
+                    name = maybe_remap_kv_scale_name(name, params_dict)
+                    if name is None:
+                        continue
+
 
                     if is_pp_missing_parameter(name, self):
                         continue
