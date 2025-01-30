ceb20f7b42c3b626ff332105aed537d35e1c9757
Lucas Wilkinson <lwilkinson@neuralmagic.com>
2025-01-30 02:29:40 +0000

    Implement MLA for deepseek v3/r1.
    Co-authored-by: Woosuk Kwon <woosuk.kwon@berkeley.edu>
    Co-authored-by: simon-mo <simon.mo@hey.com>
    Signed-off-by: Lucas Wilkinson <lwilkinson@neuralmagic.com>

--- vllm/attention/backends/abstract.py
+++ vllm/attention/backends/abstract.py
@@ -268,9 +268,25 @@ class AttentionImpl(ABC, Generic[T]):
     def forward(
         self,
         layer: AttentionLayer,
-        query: torch.Tensor,
-        key: torch.Tensor,
-        value: torch.Tensor,
+        query: torch.Tensor,  # For MLA hidden_states_or_cq
+        key: torch.Tensor,  # For MLA kv_c_normed
+        value: torch.Tensor,  # For MLA k_pe
+        kv_cache: torch.Tensor,
+        attn_metadata: T,
+        output: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        raise NotImplementedError
+
+
+class MLAAttentionImpl(AttentionImpl[T], Generic[T]):
+
+    @abstractmethod
+    def forward(
+        self,
+        layer: AttentionLayer,
+        hidden_states_or_cq: torch.Tensor,
+        kv_c_normed: torch.Tensor,
+        k_pe: torch.Tensor,
         kv_cache: torch.Tensor,
         attn_metadata: T,
         output: Optional[torch.Tensor] = None,
