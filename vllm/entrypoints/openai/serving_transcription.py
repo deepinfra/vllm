5c1b680e0478b3e30c81e86e0b9f333d09996579
Temirulan Mussayev <temirulan@deepinfra.com>
2025-02-20 14:41:18 -0800

    add logs for transcription result

--- vllm/entrypoints/openai/serving_transcription.py
+++ vllm/entrypoints/openai/serving_transcription.py
@@ -444,7 +444,7 @@ class OpenAIServingTranscription(OpenAIServing):
                 default_max_tokens, default_params
             )
             sampling_params.skip_special_tokens = False
-            sampling_params.logprobs = 1
+            sampling_params.logprobs = 2
             sampling_params.temperature = request.temperature
             sampling_params.bad_words = ["<|notimestamps|>"]
 
