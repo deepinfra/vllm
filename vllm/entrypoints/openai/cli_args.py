6567369edf021596ef6f73ceb9fcb6c7c46fea6c
Temirulan Mussayev <mtemirulan@gmail.com>
2025-03-13 00:42:26 +0000

    fix --lora-modules cli arg to accept multiple with equal sign

--- vllm/entrypoints/openai/cli_args.py
+++ vllm/entrypoints/openai/cli_args.py
@@ -34,18 +34,19 @@ class LoRAParserAction(argparse.Action):
         if isinstance(values, str):
             raise TypeError("Expected values to be a list")
 
-        lora_list: list[LoRAModulePath] = []
+        current_lora_list: list[LoRAModulePath] = getattr(namespace, self.dest, []) or []
+
         for item in values:
             if item in [None, '']:  # Skip if item is None or empty string
                 continue
             if '=' in item and ',' not in item:  # Old format: name=path
                 name, path = item.split('=')
-                lora_list.append(LoRAModulePath(name, path))
+                current_lora_list.append(LoRAModulePath(name, path))
             else:  # Assume JSON format
                 try:
                     lora_dict = json.loads(item)
                     lora = LoRAModulePath(**lora_dict)
-                    lora_list.append(lora)
+                    current_lora_list.append(lora)
                 except json.JSONDecodeError:
                     parser.error(
                         f"Invalid JSON format for --lora-modules: {item}")
@@ -53,7 +54,7 @@ class LoRAParserAction(argparse.Action):
                     parser.error(
                         f"Invalid fields for --lora-modules: {item} - {str(e)}"
                     )
-        setattr(namespace, self.dest, lora_list)
+        setattr(namespace, self.dest, current_lora_list)
 
 
 class PromptAdapterParserAction(argparse.Action):
